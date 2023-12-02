d49f70e7503ea912699a40854007ad551d5b5cbd
Patrick White <emailpatrick74@gmail.com>
2023-12-01 20:47:30 -0500

    why isn't readODS loading? just comment it out

--- R/hello.R
+++ R/hello.R
@@ -14,44 +14,44 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-library(readODS)
+# library(readODS)
 
-setup_package <- function() {
+hello <- function() {
   file_list <- list.files(path = "../aos-data/", pattern = "\\.ods$", full.names = TRUE)
-  for(file_path in file_list) {
-    # Extract the base name without extension
-    file_name <- tools::file_path_sans_ext(basename(file_path))
-
-    # Read the .ods file into a data frame
-    data_frame <- data.frame(read_ods(file_path))
-
-    # Assign the data frame to a variable named after the file
-    assign(file_name, data_frame, envir = .GlobalEnv)
-  }
+  # for(file_path in file_list) {
+  #   # Extract the base name without extension
+  #   file_name <- tools::file_path_sans_ext(basename(file_path))
+  #
+  #   # Read the .ods file into a data frame
+  #   data_frame <- data.frame(read_ods(file_path))
+  #
+  #   # Assign the data frame to a variable named after the file
+  #   assign(file_name, data_frame, envir = .GlobalEnv)
+  # }
 
   print("Hello, world!")
-  usethis::use_data( batteryLife , overwrite=TRUE)
-  usethis::use_data( batteryLife2 , overwrite=TRUE)
-  usethis::use_data( beetFertilizer , overwrite=TRUE)
-  usethis::use_data( cerealSugar , overwrite=TRUE)
-  usethis::use_data( coffeeCups , overwrite=TRUE)
-  usethis::use_data( cosmetics , overwrite=TRUE)
-  usethis::use_data( cuckooEggs , overwrite=TRUE)
-  usethis::use_data( egyptSkulls , overwrite=TRUE)
-  usethis::use_data( foodCosts , overwrite=TRUE)
-  usethis::use_data( headHeight , overwrite=TRUE)
-  usethis::use_data( healthClub , overwrite=TRUE)
-  usethis::use_data( lawmowers , overwrite=TRUE)
-  usethis::use_data( lunarCrimes , overwrite=TRUE)
-  usethis::use_data( medicaidTimes , overwrite=TRUE)
-  usethis::use_data( mmColors , overwrite=TRUE)
-  usethis::use_data( pigVitamins , overwrite=TRUE)
-  usethis::use_data( readingActivities , overwrite=TRUE)
-  usethis::use_data( shotPut , overwrite=TRUE)
-  usethis::use_data( tattoo , overwrite=TRUE)
-  usethis::use_data( titanicClass , overwrite=TRUE)
-  usethis::use_data( trainingManual , overwrite=TRUE)
-  usethis::use_data( voters , overwrite=TRUE)
-  usethis::use_data( waxTimes , overwrite=TRUE)
-  usethis::use_data( weightsHealthClub , overwrite=TRUE)
+  # usethis::use_data( batteryLife , overwrite=TRUE)
+  # usethis::use_data( batteryLife2 , overwrite=TRUE)
+  # usethis::use_data( beetFertilizer , overwrite=TRUE)
+  # usethis::use_data( cerealSugar , overwrite=TRUE)
+  # usethis::use_data( coffeeCups , overwrite=TRUE)
+  # usethis::use_data( cosmetics , overwrite=TRUE)
+  # usethis::use_data( cuckooEggs , overwrite=TRUE)
+  # usethis::use_data( egyptSkulls , overwrite=TRUE)
+  # usethis::use_data( foodCosts , overwrite=TRUE)
+  # usethis::use_data( headHeight , overwrite=TRUE)
+  # usethis::use_data( healthClub , overwrite=TRUE)
+  # usethis::use_data( lawmowers , overwrite=TRUE)
+  # usethis::use_data( lunarCrimes , overwrite=TRUE)
+  # usethis::use_data( medicaidTimes , overwrite=TRUE)
+  # usethis::use_data( mmColors , overwrite=TRUE)
+  # usethis::use_data( pigVitamins , overwrite=TRUE)
+  # usethis::use_data( readingActivities , overwrite=TRUE)
+  # usethis::use_data( shotPut , overwrite=TRUE)
+  # usethis::use_data( tattoo , overwrite=TRUE)
+  # usethis::use_data( titanicClass , overwrite=TRUE)
+  # usethis::use_data( trainingManual , overwrite=TRUE)
+  # usethis::use_data( voters , overwrite=TRUE)
+  # usethis::use_data( waxTimes , overwrite=TRUE)
+  # usethis::use_data( weightsHealthClub , overwrite=TRUE)
 }
